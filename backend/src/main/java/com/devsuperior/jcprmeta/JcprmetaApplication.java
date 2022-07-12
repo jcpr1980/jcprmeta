0c704258f2e27d184cfb91466a7053fbb518903a
Julio Pacheco <jcpribeiro@gmail.com>
2022-07-12 08:31:22 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/jcprmeta/JcprmetaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.jcprmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcprmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcprmetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/jcprmeta/JcprmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.jcprmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcprmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
