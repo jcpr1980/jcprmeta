60eaf41d1816ddbbddc72623159a31a9d5382c59
Julio Pacheco <jcpribeiro@gmail.com>
2022-07-13 06:02:38 -0300

    First component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,14 @@
+import icon from '../../assets/img/notification-icon.svg' 
+import './styles.css'
+
+function NotificationButton() {
+    return(
+        <div className="jcprmeta-red-btn">
+            <img src={icon} alt="Notificar" />
+        </div>
+
+        )
+  }
+  
+  export default NotificationButton
+  
